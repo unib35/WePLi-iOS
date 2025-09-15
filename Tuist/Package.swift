282a0f978e6951bb94843bd79efe5c0186aa9cef
Jong MIn <unib335@naver.com>
2025-09-16 03:31:47 +0900

    [Setting]: SPM 의존성 관리를 위한 Package.swift 추가
    - 프로젝트에서 사용할 외부 라이브러리 의존성 정의
    - Moya, Kingfisher, TCA 등 주요 라이브러리 포함
    - Tuist와 SPM이 함께 사용 가능하도록 설정

---
+++ Tuist/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.1
+import PackageDescription
+
+#if TUIST
+  import ProjectDescription
+  import ProjectDescriptionHelpers
+
+  let packageSettings = PackageSettings()
+#endif
+
+let package = Package(
+  name: "WePLiPackages",
+  dependencies: [
+    .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
+    .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.0.0"),
+    .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "7.0.0"),
+    .package(url: "https://github.com/supabase-community/supabase-swift.git", from: "2.0.0"),
+    .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0"),
+  ]
+)
